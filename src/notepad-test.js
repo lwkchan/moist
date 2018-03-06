2424530e748d1476f89ab2b050d306a7bd60d286
Laura Chan <laurachan205@gmail.com>
2018-03-06 16:14:09 +0000

    Rename html tester

---
+++ src/notepad-test.js
@@ -0,0 +1,24 @@
+describe('Notepad', function() {
+  var notepad;
+  before.beforeEach(function() {
+    notepad = new Notepad();
+  });
+  it('----> it is empty*', function() {
+    expect.toBeEmpty(notepad.content);
+  });
+  it('----> it is empty**', function() {
+    notepad.content.push('ehehhehhe');
+    expect.toBeEmpty(notepad.content);
+  });
+  it('----> it is empty***', function() {
+    expect.toBeEmpty(notepad.content);
+  });
+  it('----> it includes hehehe', function() {
+    notepad.content.push('hehehe');
+    expect.toInclude(notepad.content, 'hehehe');
+  });
+  it('----> it includes hihihi', function() {
+    notepad.content.push('hehehe');
+    expect.toInclude(notepad.content, 'hihihi');
+  });
+});
