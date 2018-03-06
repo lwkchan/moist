1e2edf8b711c0dd82043e3f7a74151cb3b69e32a
kotpsot <zygmunt.justyna@gmail.com>
2018-03-06 09:51:14 +0000

    in expect.js add console.log to all messages.

--- expect.js
+++ expect.js
@@ -1,12 +1,12 @@
 const expect = { //vars are accessible everywhere, even when module.
   toEqual: function(assert, expect) {
-    return (assert === expect ? "Equality test passed." : (assert + " does not equal " + expect + ". The test failed."))
+    return (assert === expect ? console.log("Equality test passed.") : (console.log(assert + " does not equal " + expect + ". The test failed.")))
   },
   toInclude: function(assert, expect) {
-    return (assert.includes(expect) ? "Inclusion test passed" : ("The tested object does not include " + expect + ". The test failed."))
+    return (assert.includes(expect) ? console.log("Inclusion test passed") : console.log("The tested object does not include " + expect + ". The test failed."))
   },
   toBeTrue: function(assert) {
-    return (assert === true ? "Truth test passed" : ("The statement is not truthy. The test failed."))
+    return (assert === true ? console.log("Truth test passed") : console.log("The statement is not truthy. The test failed."))
   },
   toBeEmpty: function(assert) {
     return (assert.length === 0 ? console.log("The assertion is empty, test passed.") : console.log("The assertion is not empty, test failed."))
